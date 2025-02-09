2adbb2b295ede2541d57118f06fd73eb75316e3b
Sounak <sounaksaha333@gmail.com>
2025-02-10 01:51:54 +0530

    0.1 => http GET request call

---
+++ springboot-rest-api/src/main/java/com/springboot/springboot/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.springboot.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+// make Java class as a Spring MVC controller
+//@ResponseBody
+// object serialized into JSON and passed back into the HttpResponse object
+@RestController
+public class HelloWorldController {
+    // HTTP GET Request
+    // http://localhost:8080/hello-world
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
