e14c29f6ec421500b54dd929b6b28370d3a53ed3
Sounak <sounaksaha333@gmail.com>
2025-02-10 02:59:24 +0530

    0.2 => Create Spring Boot REST API with Path Variable

--- springboot-rest-api/src/main/java/com/springboot/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/com/springboot/springboot/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.springboot.springboot.controller;
 
 import com.springboot.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @RestController
 public class StudentController {
+    // http://localhost:8080
 
     @GetMapping("/student")
     public Student getStudent() {
@@ -22,5 +24,14 @@ public class StudentController {
         students.add(new Student(2, "Sounak", "Saha"));
         students.add(new Student(3, "Jai", "Gupta"));
         return students;
+    }
+
+    // Spring Boot REST API with Path Variable
+    @GetMapping("/student/{id}/{first-name}/{last-name}")
+    // {id} -> URI template variable
+    public Student studentPathVariable(@PathVariable("id") int studentId,
+                                       @PathVariable("first-name") String firstName,
+                                       @PathVariable("last-name") String lastName) {
+        return new Student(studentId, firstName, lastName);
     }
 }
